a2d75e11eea3f71d89b4d95239d8c7825f58125f
Kanis Mim <kanismim075@gmail.com>
2019-11-02 02:12:15 +0600

    added login and home

---
+++ controllers/login.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var userModel = require('./../models/user-model');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+	res.render('login/index');
+});
+
+router.post('/', function(req, res){
+
+	
+
+	var user = {
+		username: req.body.username,
+		password: req.body.password
+	}
+console.log(user.username);
+	userModel.validate(user, function(status){
+		
+		if(status==1){
+			res.cookie('username', req.body.uname);
+			res.redirect('/home');	
+		}else if(status==2) {
+			res.cookie('username', req.body.uname);
+			res.redirect('/home');	}
+			else{
+			res.send('invalid username/password');
+		}
+	});
+
+});
+
+
+
+
+module.exports = router;
+
+
--- models/db.js
+++ models/db.js
@@ -1,82 +1,58 @@
-var mysql = require('mysql');
+var mysql   = require('mysql');
 
-var getConnection = function () {
+var getConnection = function(){
 
 	var connection = mysql.createConnection({
-		host: 'localhost',
+		host:'localhost',
 		user: 'root',
 		password: '',
-		database: 'cloth_shop'
+		database: 'hiring'
 	});
-
-	connection.connect(function (err) {
-		if (err) {
-			console.error('error connecting: ' + err.stack);
-			return;
-		}
-		console.log('connected as id ' + connection.threadId);
+	 
+	connection.connect(function(err) {
+	  if (err) {
+	    console.error('error connecting: ' + err.stack);
+	    return;
+	  }
+	  console.log('connected as id ' + connection.threadId);
 	});
 
 	return connection;
 }
 
-module.exports = {
 
-	getResults: function (sql, params, callback) {
+module.exports={
 
-		if (params != null) {
-			var connection = getConnection();
-			connection.query(sql, params, function (error, results) {
-				callback(results);
-			});
+	getResults: function(sql, callback){
 
-			connection.end(function (err) {
-				console.log("Database connection is terminated.");
-			});
+		var connection = getConnection();
+		connection.query(sql, function(error, results){
 
-		} else {
+			callback(results);
+		});
 
-			var connection = getConnection();
-			connection.query(sql, function (error, results) {
-				callback(results);
-			});
-
-			connection.end(function (err) {
-				console.log("Database connection is terminated.");
-			});
-		}
+		connection.end(function(err) {
+		  console.log("Database connection is terminated.");
+		});
 	},
-	execute: function (sql, params, callback) {
+	execute: function(sql, callback){
 
-		if (params != null) {
-			var connection = getConnection();
-			connection.query(sql, params, function (error, results) {
+		var connection = getConnection();
+		connection.query(sql, function(error, results){
 
-				if (error) {
-					callback(false);
-				} else {
-					callback(true);
-				}
-			});
+			if(error){
+				callback(false);
+			}else{
+				callback(true);
+			}
 
-			connection.end(function (err) {
-				console.log("Database connection is terminated.");
-			});
-		} else {
-			var connection = getConnection();
-			connection.query(sql, function (error, results) {
+		});
 
-				if (error) {
-					callback(false);
-				} else {
-					callback(true);
-				}
-			});
-
-			connection.end(function (err) {
-				console.log("Database connection is terminated.");
-			});
-		}
-	}
+		connection.end(function(err) {
+		  console.log("Database connection is terminated.");
+		});
+	},
 }
+
+
 
