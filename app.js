38bf32984557b1dfa5be25ce8d42fefb10e4f21f
MahdiFaheem <mahdihasanfaheem@gmail.com>
2019-11-02 19:39:50 +0600

    customer home

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 //DECLARATION
-var express  	= require('express');
-var ejs  		= require('ejs');
-var bodyParse  	= require('body-parser');
-var exSession  	= require('express-session');
-var cookieParser= require('cookie-parser');
-var home  		= require('./controllers/home');
+var express = require('express');
+var ejs = require('ejs');
+var bodyParse = require('body-parser');
+var exSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var home = require('./controllers/home');
+var customer = require('./controllers/customer');
+var login = require('./controllers/login');
 
-var login  		= require('./controllers/login');
-
- const path = require('path');
-var app 		= express();
+const path = require('path');
+var app = express();
 
 //CONGIFURATION
 app.set('view engine', 'ejs');
 
 //MIDDLEWARE
-app.use(bodyParse.urlencoded({extended:false}));
-app.use(exSession({secret:"my top secret value", saveUninitialized:true, resave:false}));
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(exSession({ secret: "my top secret value", saveUninitialized: true, resave: false }));
 app.use(cookieParser());
 app.use('/home', home);
 app.use('/login', login);
-
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/customer', customer);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 
 //ROUTING
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.send('<h2>hello from express</h2>');
 });
 
 
 //SERVER STARTUP
-app.listen(3000, function(){
+app.listen(3000, function () {
 	console.log('server started at 3000...');
 });
---
+++ models/customer-model.js
@@ -0,0 +1,67 @@
+var db = require('./db');
+
+module.exports = {
+
+    getById: function (id, callback) {
+
+        var sql = "select * from user where id=?";
+        db.getResults(sql, [id], function (result) {
+
+            //console.log(result);
+            if (result.length > 0) {
+                callback(result[0]);
+            } else {
+                callback([]);
+            }
+        });
+    },
+
+
+
+
+    validate: function (user, callback) {
+        var sql = "select * from user where username=? and password=?";
+
+        db.getResults(sql, [user.username, user.password], function (result) {
+
+            if (result.length > 0) {
+                callback(true);
+            } else {
+                callback(false);
+            }
+        });
+    },
+    getAll: function (callback) {
+        var sql = "select * from user where type=2";
+
+        db.getResults(sql, [], function (results) {
+
+            if (results.length > 0) {
+                callback(results);
+            } else {
+                callback([]);
+            }
+        });
+    },
+
+
+
+    update: function (user, callback) {
+        var sql = "update user set username=?, password=? where id=?";
+        db.execute(sql, [user.username, user.password, user.id], function (status) {
+            callback(status);
+        });
+
+    },
+
+    delete: function (id, callback) {
+        var sql = "delete from user where id=?";
+        db.execute(sql, [id], function (status) {
+            callback(status);
+        });
+
+    }
+
+}
+
+
