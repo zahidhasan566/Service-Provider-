40ffe115978d0f89666cccffdf04cb1a0d37e58c
Kanis Mim <kanismim075@gmail.com>
2019-11-04 11:08:14 +0600

    customer view

--- controllers/admin.js
+++ controllers/admin.js
@@ -51,9 +51,6 @@ router.post('/update/:id', function(req, res){
 		city: req.body.city,
 		id: req.params.id
 	};
-
-	
-
 	userModel.update(user, function(status){
 		console.log(status);
 		if(status){
@@ -63,11 +60,145 @@ router.post('/update/:id', function(req, res){
 		}
 	});
 });
-router.get('/details/:id', function(req, res){
 
-	userModel.getById(req.params.id, function(result){
-		res.render('/user/details', {user: result});
+
+router.get('/providerupdate/:id', function(req, res){
+
+	userModel.getById(req.params.id, function(results){
+		res.render('admin/providerupdate', {user: results});		
+	});
+
+});
+
+router.post('/providerupdate/:id', function(req, res){
+	
+	var user = {
+		
+		skill: req.body.skill,
+		id: req.params.id
+	};
+	userModel.proupdate(user, function(status){
+		console.log(status);
+		if(status){
+			res.redirect('/adminhome');
+		}else{
+			res.redirect('/adminhome');
+		}
 	});
 });
+router.get('/warning/:id', function(req, res){
+
+	userModel.getById(req.params.id, function(result){
+		res.render('admin/warning', {user: result});
+	});
+});
+
+router.post('/warning/:id', function(req, res){
+	userModel.getprofile(function(results){
+console.log(results[0].userid);
+var user = {
+		
+	warning: req.body.warning,
+	userid: results[0].userid,
+	id: req.params.id
+};
+	userModel.warning(user, function(status){
+		if(status){
+			res.redirect('/adminhome');
+		}else{
+			res.redirect('/adminhome');
+		}
+	});
+});
+});
+
+router.get('/message/:id', function(req, res){
+
+	userModel.getById(req.params.id, function(result){
+		res.render('admin/message', {user: result});
+	});
+});
+
+router.post('/message/:id', function(req, res){
+	userModel.getprofile(function(results){
+console.log(results[0].userid);
+var user = {
+		
+	message: req.body.message,
+	userid: results[0].userid,
+	id: req.params.id
+};
+	userModel.message(user, function(status){
+		if(status){
+			res.redirect('/adminhome');
+		}else{
+			res.redirect('/adminhome');
+		}
+	});
+});
+});
+
+router.get('/disable/:id', function(req, res){
+
+	userModel.disable(req.params.id, function(status){
+		if(status){
+			res.redirect('/adminhome');
+		}else{
+			res.redirect('/adminhome');
+		}
+	});
+});
+
+
+
+
+router.get('/customer', function(req, res){
+
+	userModel.getcustomer(function(results){
+		
+			res.render('admin/customer', {user: results});
+		
+	});
+});
+
+
+
+router.get('/customerinfo/:id', function(req, res){
+
+	userModel.getByIdcus(req.params.id, function(results){
+
+		res.render('admin/customerinfo', {user: results});		
+	});
+
+});
+
+
+router.get('/cuswarning/:id', function(req, res){
+
+	userModel.getByIdcus(req.params.id, function(result){
+		res.render('admin/warning', {user: result});
+	});
+});
+
+router.post('/cuswarning/:id', function(req, res){
+	userModel.getprofile(function(results){
+console.log(results[0].userid);
+var user = {
+		
+	warning: req.body.warning,
+	userid: results[0].userid,
+	id: req.params.id
+};
+	userModel.warning(user, function(status){
+		if(status){
+			res.redirect('/adminhome');
+		}else{
+			res.redirect('/adminhome');
+		}
+	});
+});
+});
+
+
 
 module.exports = router;
